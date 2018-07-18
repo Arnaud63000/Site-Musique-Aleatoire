4e68b64c2e8bdce433db73c2bd2d5f7894037d5b
Arnaud63000 <40628189+Arnaud63000@users.noreply.github.com>
2018-07-18 18:55:32 +0200

    Update Starsky.php

--- site musique aleatoire/Starsky.php
+++ site musique aleatoire/Starsky.php
@@ -1,3 +1,6 @@
+<?php
+	session_start(); 
+?>
 <!doctype html>
 <html>
 
@@ -19,15 +22,12 @@
 		<embed src="musiques/Générique - Starsky  Hutch.mp3" controller="true" autoplay="true" autostart="True" type="audio/mp3" />
 	</object>
     <?php 
-
-
 $urls = array("AgenceToutRisque.php" ,"Starsky.php" ,"pokemon.php" ,"stupeflip.php" ,"starwars.php" ,"dallas.php" ,"indiana.php"); 
-
-
   $random = (rand()%7); 
-header("Location: ".$url);
-exit;
-
+		if("/".$_SERVER['REQUEST_URI'] !== $urls[$random])
+	{
+		header("Location: ".$urls[$random]);
+	}
 ?>
 </body>
 
