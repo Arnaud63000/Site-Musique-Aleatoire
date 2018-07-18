688e31d588a326ad262a9e15403553836b99d495
Arnaud63000 <40628189+Arnaud63000@users.noreply.github.com>
2018-07-18 18:55:50 +0200

    Update dallas.php

--- site musique aleatoire/dallas.php
+++ site musique aleatoire/dallas.php
@@ -1,3 +1,6 @@
+<?php
+	session_start(); 
+?>
 <!doctype html>
 <html>
 
@@ -5,29 +8,26 @@
 	
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/style.css">
-    <title>Dallas !</title>
+    <title>Starsky Et Hutch !</title>
 </head>
 
 <body>
-	<img src="images/DALLAS_preview.jpg" id="bg" alt="photo">
-	
+	<img src="images/starsky.jpg" id="bg" alt="photo">
+
 	<object>
 		<param name="autostart" value="true">
 		<param name="src" value="sound.mp3">
 		<param name="autoplay" value="true"> 
 		<param name="controller" value="true">
-		<embed src="musiques/Générique dallas (premier).mp3" controller="true" autoplay="true" autostart="True" type="audio/mp3" />
+		<embed src="musiques/Générique - Starsky  Hutch.mp3" controller="true" autoplay="true" autostart="True" type="audio/mp3" />
 	</object>
     <?php 
-
-
 $urls = array("AgenceToutRisque.php" ,"Starsky.php" ,"pokemon.php" ,"stupeflip.php" ,"starwars.php" ,"dallas.php" ,"indiana.php"); 
-
-
   $random = (rand()%7); 
-header("Location: ".$url);
-exit;
-
+		if("/".$_SERVER['REQUEST_URI'] !== $urls[$random])
+	{
+		header("Location: ".$urls[$random]);
+	}
 ?>
 </body>
 
